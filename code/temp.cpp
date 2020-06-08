f32bbad4cbc6e0431968298c32f13be09fd22393
Bernardo Amorim <bernardotamorim@gmail.com>
2020-06-08 10:14:51 -0300

    Fixed mistakes in proof, and added new code

--- code/temp.cpp
+++ code/temp.cpp
@@ -4,12 +4,10 @@ using namespace std;
 
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
-#define ff first
-#define ss second
-#define pb push_back
+#define f first
+#define s second
 
 typedef long long ll;
-typedef pair<int, int> pii;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
